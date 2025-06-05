76ad7436cc8deb947f2eb1299075a465516e9dcc
299792455 <75017pi@gmail.com>
2025-06-05 17:26:42 +0200

    Added Services Section - Issue : Img not relevant

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "../components/Hero";
 import Background from "@/components/Background";
+import Services from "@/components/Services";
 
 export default function Home() {
   return (
     <main className="bg-black text-white font-['Space_Grotesk'] overflow-x-hidden">
       <Background />
       <Hero />
+      <Services />
     </main>
   );
 }

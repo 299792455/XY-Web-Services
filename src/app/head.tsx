d5f701c62709d3b2ee047791dc01cd002c13ae13
299792455 <75017pi@gmail.com>
2025-06-27 20:52:33 +0200

    Added : head.tsx + google verif

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -3,6 +3,7 @@ export default function Head() {
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
+      <meta name="google-site-verification" content="rXN2yiF_y6qpvs24uDt53ij6zhhu3sUEYYky6RykBw8" />
       <link rel="icon" href="/favicon.ico" />
     </>
   );
